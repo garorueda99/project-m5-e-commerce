fc88c3ecc6b37488d9cfed41f08bac798e879e93
Andres Rueda <garo99@gmail.com>
2020-09-12 11:07:51 -0400

    Let's start

---
+++ client/src/reducers/example-reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  exampleID: null,
+  exampleProducts: null,
+  exampleValue: null,
+  status: "idle",
+};
+
+export default function artistReducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
---
+++ client/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+//CREATE INDEPENDENT STATES FOR NEW THINGS ARE NOT CREATED. if doubts ask Andres Rueda
+import example from "./example-reducer";
+
+export default combineReducers({ example });
