9646c39f1b4781710750ea98a8a4bfe27aec13d2
n4dz <nadzamini@gmail.com>
2020-09-12 15:57:21 -0400

    Added router to app component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './homepage/Home';
+import Cart from './cartpage/Cart';
+import Item from './itempage/Item';
+import Error from './errorpage/Error';
 
 function App() {
   const [bacon, setBacon] = useState(null);
 
   useEffect(() => {
-    fetch("/bacon")
+    fetch('/bacon')
       .then((res) => res.json())
       .then((data) => setBacon(data));
   }, []);
 
-  return <div>{bacon ? bacon : `...LET's START?...`}</div>;
+  return (
+    <div>
+      {bacon ? (
+        bacon
+      ) : (
+        <Router>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/item/:itemId">
+            <Item />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/error">
+            <Error />
+          </Route>
+        </Router>
+      )}
+    </div>
+  );
 }
 
 export default App;
