db838469a3179b3d351a70a4bf67f474deeb8a7b
Andres Rueda <garo99@gmail.com>
2020-09-18 19:36:26 -0400

    correction to Carl's review

--- client/src/components/home-page/PageIndex.js
+++ client/src/components/home-page/PageIndex.js
@@ -3,16 +3,9 @@ import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { BsCaretRight } from 'react-icons/bs';
-<<<<<<< HEAD
 import { nextPageItems } from '../../actions';
 
 export default function PageIndex({ page, setPage }) {
-=======
-import { fetchItems } from '../helpers/fetch-functions';
-import { requestItems, receiveItemsInfo } from '../../actions';
-
-export default function PageIndex() {
->>>>>>> 3c4465f4fe3826ac68989b2b3de57d3b1a19b87e
   const dispatch = useDispatch();
   const items = useSelector((state) => state.items);
   const lastPage = Math.round(items.totalFound / items.pageSize);
@@ -31,25 +24,6 @@ export default function PageIndex() {
       </Button>
     </Wrapper>
   );
-<<<<<<< HEAD
-=======
-  return (
-    <Wrapper>
-      Page 1 of {lastPage}
-      <Button
-        onClick={() => {
-          dispatch(requestItems());
-          fetchItems({
-            ...items.filters,
-            initial_index: items.nextIndex,
-          }).then((res) => dispatch(receiveItemsInfo(res)));
-        }}
-      >
-        <BsCaretRight color={'gray'} />
-      </Button>
-    </Wrapper>
-  );
->>>>>>> 3c4465f4fe3826ac68989b2b3de57d3b1a19b87e
 }
 
 const Wrapper = styled.div`
